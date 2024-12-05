83f445b055cf2551fc7b2cd2db41b245e51b02cd
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-05 17:15:31 +0100

    feat: ex 4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -5,9 +5,12 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+const sentence = (name, word) => (`${word} ${userName}`);
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const wordAddname = sentence(userName, "Ciao")
+console.log(wordAddname)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
