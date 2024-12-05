1c196da9e41d610106a153c86ab6f655bd17c9dc
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-05 14:44:24 +0100

    feat: ex 1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,29 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+/**
+ * Check per avere solo il primo caratere da un elemento di un array
+ *
+ * @param {array} Il array iniziale
+ * @returns {array} Il risultato e solo il primo caratere di un elemnto del array
+ * 
+ */
+
+const isLetInitial = (array) => {
+        let initialLet = [];
+        for(let i = 0; i <= names.length - 1; i++) {   
+        initialLet.push(names[i][0])     
+    }
+    return initialLet
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const letInitialsArray = isLetInitial(names)
+console.log(letInitialsArray)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
